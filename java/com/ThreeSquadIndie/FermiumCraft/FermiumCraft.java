62f61472fdfdad25a7f44775b96a407f4977102d
dethsanius <sindre.s.brurberg@gmail.com>
2014-02-09 16:10:20 +0100

    Dwv envirement and basic structur of the project compleated now it is the part of adding code.

---
+++ java/com/ThreeSquadIndie/FermiumCraft/FermiumCraft.java
@@ -0,0 +1,19 @@
+package com.ThreeSquadIndie.FermiumCraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+/**
+ * Created by Haxer on 09.02.14.
+ */
+@Mod(modid = Reference.MODID, version = Reference.VERSION)
+public class FermiumCraft {
+
+
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
---
+++ java/com/ThreeSquadIndie/FermiumCraft/Reference.java
@@ -0,0 +1,10 @@
+package com.ThreeSquadIndie.FermiumCraft;
+
+/**
+ * Created by Haxer on 09.02.14.
+ */
+public class Reference {
+    public static final String MODID = "fermiumcraft";
+    public static final String VERSION = "0.01";
+
+}
