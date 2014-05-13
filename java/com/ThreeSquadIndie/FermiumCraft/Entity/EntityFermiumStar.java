8c4e601ba9efd82e0c90f6d814fc337889912e99
dethsanius <sindre.s.brurberg@gmail.com>
2014-05-13 23:16:12 +0200

    fixing lagg in explotions

--- java/com/ThreeSquadIndie/FermiumCraft/Entity/EntityFermiumStar.java
+++ java/com/ThreeSquadIndie/FermiumCraft/Entity/EntityFermiumStar.java
@@ -61,6 +61,92 @@ public class EntityFermiumStar extends EntityThrowable {
         this.worldObj.createExplosion(this, this.posX + math, this.posY - math, this.posZ + math, (float)5, true);
     }
 
+    private void left(int math){
+        this.worldObj.createExplosion(this, this.posX + math, this.posY - math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY - math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY - math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY + math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY + math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY + math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY, this.posZ + math, (float)5, true);
+    }
+
+    private void middle(int math){
+        this.worldObj.createExplosion(this, this.posX, this.posY - math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY - math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY - math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY + math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY + math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY + math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ + math, (float)5, true);
+    }
+
+    private void right(int math){
+        this.worldObj.createExplosion(this, this.posX - math, this.posY - math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY - math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY - math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY + math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY + math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY + math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY, this.posZ - math, (float)5, true);
+    }
+
+    private void kvadrant1(int math){
+        this.worldObj.createExplosion(this, this.posX - math, this.posY - math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY + math, this.posZ - math, (float)5, true);
+    }
+
+    private void kvadrant2(int math){
+        this.worldObj.createExplosion(this, this.posX - math, this.posY - math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY + math, this.posZ + math, (float)5, true);
+    }
+
+    private void kvadrant3(int math){
+        this.worldObj.createExplosion(this, this.posX + math, this.posY - math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY + math, this.posZ - math, (float)5, true);
+    }
+
+    private void kvadrant4(int math){
+        this.worldObj.createExplosion(this, this.posX + math, this.posY - math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY + math, this.posZ + math, (float)5, true);
+    }
+
+    private void kvadrant5(int math){
+        this.worldObj.createExplosion(this, this.posX, this.posY - math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY + math, this.posZ, (float)5, true);
+    }
+
+    private void kvadrant6(int math){
+        this.worldObj.createExplosion(this, this.posX, this.posY - math, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ + math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY + math, this.posZ + math, (float)5, true);
+    }
+
+    private void kvadrant7(int math){
+        this.worldObj.createExplosion(this, this.posX, this.posY - math, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ - math, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX, this.posY + math, this.posZ - math, (float)5, true);
+    }
+
+    private void kvadrant8(int math){
+        this.worldObj.createExplosion(this, this.posX - math, this.posY - math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX - math, this.posY + math, this.posZ, (float)5, true);
+    }
+
+    private void kvadrant9(int math){
+        this.worldObj.createExplosion(this, this.posX + math, this.posY - math, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX + math, this.posY, this.posZ, (float)5, true);
+        this.worldObj.createExplosion(this, this.posX +  math, this.posY + math, this.posZ, (float)5, true);
+    }
+
 
     /**
      * Called when this EntityThrowable hits a block or entity.
@@ -78,28 +164,108 @@ public class EntityFermiumStar extends EntityThrowable {
             par1MovingObjectPosition.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), (float)b0);
         }
         int num = 0;
-        int var = 5;
-        if (!this.worldObj.isRemote)
-        {
-            for (int i = 0; i < 1000; ++i)
+        int length = 100000;
+        int var = length / 100;
+        if (!this.worldObj.isRemote) {
+            this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+            this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ, (float)5, true);
+            this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+            for (int i = 0; i <= length; ++i)
             {
                 int math = i / var;
-                if (i == 0){
-                    this.worldObj.createExplosion(this, this.posX, this.posY, this.posZ, (float)5, true);
-                }
-                if (num == var - 4){
-                    up(math);
-                }
-                else if (num == var - 2){
-                    flat(math);
-                }
-                else if (num == var){
-                    down(math);
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant1(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
                     num = 0;
                 }
-                if (i == 999){
-
-                    this.worldObj.newExplosion(this, this.posX, this.posY, this.posZ, (float)5, true, true);
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant2(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
+                }
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant3(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
+                }
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant4(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
+                }
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant5(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
+                }
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant6(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
+                }
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant7(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
+                }
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant8(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
+                }
+                num++;
+            }
+            for (int i = 0; i <= length; ++i)
+            {
+                int math = i / var;
+                if (num == var){
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "false");
+                    kvadrant9(math);
+                    this.worldObj.getGameRules().setOrCreateGameRule("doTileDrops", "true");
+                    num = 0;
                 }
                 num++;
             }
